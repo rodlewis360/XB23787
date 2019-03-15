af2f56ff21e1fbfc24b92dbda2a4cc9e2e8defba
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-03-15 08:24:06 -0700

    Update XB23787learn.py

--- XB23787learn.py
+++ XB23787learn.py
@@ -1,1 +1,8 @@
-
+QuestionAndResponse = {'What is your name?' : 'XB23787', 'Do you like humans?' : 'No.'}
+def XB23787(QuestionAndResponse):
+   import random
+   print("I am a chatbot, programmed to do your work.")
+   player = input()
+   if not player in QuestionAndResponse:
+       print("Adding question to database...")
+       QuestionAndResponse
